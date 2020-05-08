ce93d2b3959e33db77ee0ee1eef5e67f4e2cbcd4
Terence Stone <terence.stone25@gmail.com>
2020-05-08 00:51:45 -0400

    adding user code to model

--- models.py
+++ models.py
@@ -1,2 +1,19 @@
 from sqlalchemy.sql import func
 from config import *
+
+#database build and methods
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+PW_REGEX = re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$')
+
+class User(bd.Model):
+    __tablename__ = "users"
+    id = bd.Column(bd.Integer, primary_key = True)
+    name = db.Column(db.String(90))
+    email = db.Column(db.String(45))
+    phone = db.Column(db.String(45))
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
