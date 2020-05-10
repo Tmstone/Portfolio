715708c5ee4be88490844cfdbeaac87fa6a9db36
Terence Stone <terence.stone25@gmail.com>
2020-05-09 23:36:37 -0400

    making modules unique to the portfolio

--- models.py
+++ models.py
@@ -6,28 +6,29 @@ from config import *
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 PW_REGEX = re.compile('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$')
 
-class User(bd.Model):
-    __tablename__ = "users"
+class Contact(bd.Model):
+    __tablename__ = "contact"
     id = bd.Column(bd.Integer, primary_key = True)
     name = db.Column(db.String(90))
     email = db.Column(db.String(45))
     phone = db.Column(db.String(45))
+    message = db.Column(db.String(225))
     created_at = db.Column(db.DateTime, server_default=func.now())
     updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
     
     def __repr__(self):
-        return '<User {}>'.format(self.name)
+        return '<Contact {}>'.format(self.name)
 
     @classmethod
-    def validate_user(cls, email):
+    def validate_contact(cls, email):
         errors=[]
-        existing_users=cls.query.filter(cls.email==email).count()
-        if (existing_users)>0:
+        existing_contact=cls.query.filter(cls.email==email).count()
+        if (existing_contact)>0:
             errors.append('This email address is already registered!')
         return errors
 
     @classmethod
-    def validate_name(cls, first_name, last_name):
+    def validate_name(cls, name):
         errors=[]
         if len(name) < 2:
             errors.append('Please enter a valid name (*at least three characters*).')
@@ -48,15 +49,21 @@ class User(bd.Model):
         return errors
     
     @classmethod
-    def validate_user(cls, attendee_info):
+    def validate_user(cls, contact_info):
         errors = []
-        errors += cls.validate_name(attendee_info['first_name'], attendee_info['last_name'])
-        errors += cls.validate_email(attendee_info['email'])
-        errors += cls.validate_password(attendee_info['password'], attendee_info['confirm_password'])
-        errors += cls.validate_phone(attendee_info['phone'])
-        errors += cls.validate_school(attendee_info['school'])
-        errors += cls.validate_graduation(attendee_info['graduation'])
-        errors += cls.validate_parent(attendee_info['parent_first'], attendee_info['parent_last'] )
-        errors += cls.validate_parent_email(attendee_info['parent_email'])
-        errors += cls.validate_parent_phone(attendee_info['parent_phone'])
+        errors += cls.validate_name(contact_info['name'])
+        errors += cls.validate_email(contact_info['email'])
+        errors += cls.validate_phone(contact_info['phone'])
         return errors
+    
+    @classmethod
+    def add_contact(cls, contact_info):    
+        new_contact = cls (
+            first_name=contact_info['name'], 
+            email=contact_info['email'], 
+            phone=contact_info['phone'],
+            message=contact_info['message']
+            )
+        db.session.add(new_attendee)
+        db.session.commit()
+        return new_contact
